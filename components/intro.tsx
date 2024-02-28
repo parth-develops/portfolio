02d7cb02160c6898262e9d5d35ac5434d5d9e0cb
Parth Umrania <pumrania9@gmail.com>
2024-02-28 12:26:27 +0530

    Developed intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,6 +3,9 @@
 import Image from "next/image";
 import propic from "@/assets/images/propic.jpg";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
@@ -35,7 +38,6 @@ export default function Intro() {
       <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-        
       >
         <span className="font-bold">{`Hello, I'm Parth.`}</span> I&apos;m a{" "}
         <span className="font-bold">front-end developer</span> with{" "}
@@ -43,6 +45,35 @@ export default function Intro() {
         building <span className="italic">web apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
+      <motion.div className="flex gap-3 justify-center items-center flex-wrap font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-800 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a
+          href="/resume.pdf"
+          download
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full shadow outline-none focus:scale-110 hover:scale-110 active:scale-105 transition"
+        >Download Resume <HiDownload className="opacity-60 group-hover:translate-y-1 transition" /></a>
+        <div className="flex justify-center gap-3">
+          <a
+            href="https://www.linkedin.com/in/parth-umrania/"
+            target="_blank"
+            className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full shadow outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition"
+          ><BsLinkedin /></a>
+          <a
+            href="https://github.com/parth-develops"
+            target="_blank"
+            className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full shadow  outline-none focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition"
+          ><BsGithub /></a>
+        </div>
+      </motion.div>
     </section>
   )
 }
