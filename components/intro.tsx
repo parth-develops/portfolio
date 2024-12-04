644cfac7eb0b08d33e434ad92c69cb7e7451c4c6
Parth Umrania <pumrania9@gmail.com>
2024-12-05 00:02:28 +0530

    Updated intro section text

--- components/intro.tsx
+++ components/intro.tsx
@@ -50,7 +50,7 @@ export default function Intro() {
       <motion.p className="mb-10 px-0 sm:px-10"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
-      ><span className="font-bold">I&apos;m a front-end developer</span> with over <span className="font-bold">a year</span> of experience. I enjoy building <span className="italic">web apps</span>. My focus is <span className="underline">React (Next.js)</span>.</motion.p>
+      ><span className="font-bold">I&apos;m a full-stack developer</span> with <span className="font-bold">2+ years</span> of experience. I enjoy building <span className="italic">web apps</span>. My focus is <span className="underline">React (Next.js)</span>.</motion.p>
       <motion.div className="flex gap-3 justify-center items-center flex-wrap font-medium mb-8"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
