587adf7eef341bcf90640756a521bb09f2f94aa6
Parth Umrania <pumrania9@gmail.com>
2025-07-01 23:00:18 +0530

    Updated meta title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import ThemeContextProvider from "@/context/theme-context";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Full-Stack JavaScript Developer | React, Next.js & Performance Optimization",
-  description: "Parth is an experienced full-stack JavaScript developer specializing in React, Next.js, and performance optimization. Passionate about building scalable web applications, enhancing site performance, and creating seamless user experiences. Explore my projects and expertise.",
+  title: "Full-Stack JavaScript Developer | React, Next.js, JavaScript, TypeScript, PostgreSQL",
+  description: "Parth is an experienced full-stack JavaScript developer specializing in React, Next.js, Typescript, PostgreSQL and performance optimization. Passionate about building scalable and type-safe web applications, enhancing site performance, and creating seamless user experiences. Explore my projects and expertise.",
 };
 
 export default function RootLayout({
